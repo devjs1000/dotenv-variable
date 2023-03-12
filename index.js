c6b46917511f33befdba319f06c212066114aa7e
anand pandit <atim4077@gmail.com>
2023-03-12 16:07:36 +0530

    cleaned

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-import { config } from "./mykeys.js";
-import test from './test.js'
-config()
+import { readFileSync } from "fs";
+import { parse } from "yaml";
+const { env } = process;
 
-console.log(process.env.port)
+export const config = async () => {
+  const configs = readFileSync(`${process.cwd()}/.env`, {
+    encoding: "utf8",
+  });
+  const parsed = parse(configs);
+  const isProduction = parsed.isProduction == undefined ? false : true;
+  const myConfig = isProduction ? parsed.production : parsed.dev;
+  for (const key in myConfig) {
+    env[key] = myConfig[key];
+  }
+};
